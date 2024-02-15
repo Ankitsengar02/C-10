8399c2b983f42dd02e3ae4fdc88c2026502e4975
Ankit Sengar <ankit.sengar@genzeon.com>
2024-02-15 18:21:39 +0530

    commit1

---
+++ Program.cs
@@ -0,0 +1,170 @@
+﻿
+// int[] numbers = { 11,22,33,44 };
+// var maxNumber = (from num in numbers
+//                          select num)
+//                         .Max();
+
+// Console.WriteLine("The maximum number is: " + maxNumber);
+//     public class Ankit
+//     {
+//         public int Ab;
+//     }
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Ankit a = new Ankit();
+//         a.Ab = 9;
+//         Console.WriteLine(a.Ab);
+//     }
+// }
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int x = 12 * 30; // Statement 1
+//         Console.WriteLine (x); // Statement 2
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int x = 12 * 30; // Statement 1
+//         Console.WriteLine (x); // Statement 2
+//     }
+// }
+
+// Console.WriteLine (FeetToInches (30)); // 360
+// Console.WriteLine (FeetToInches (100)); // 1200
+// int FeetToInches (int feet)
+// {
+//  int inches = feet * 12;
+//  return inches;
+// }
+
+// class Program {
+//     public void FeetToInches(int a){
+//         Console.WriteLine(a*12);
+//     }
+//     static void Main(string[] args){
+//         Program p= new Program();
+//         p.FeetToInches(30);
+//     }
+// }
+
+// class Program {
+//     //public int FeetToInches(int a){
+//     // public static int FeetToInches(int a){
+//     public static string FeetToInches(string a){
+//         string p=a + " " + "Net";
+//         return p;
+//     }
+//     static void Main(string[] args){
+//         //Program p= new Program();
+//         // int a = FeetToInches(20);
+//         string b = FeetToInches("dot");
+//         Console.WriteLine(b);
+//     }
+// }
+
+// class Program1 {
+//     static void Main(string[] args){
+//         string a="ankit";
+//         string b= a.ToUpper();
+//         Console.WriteLine(a);
+//         Console.WriteLine(b);
+
+//         int x=2024;
+//         a=a + x.ToString();
+//         Console.WriteLine(a);
+//     }
+// }
+
+// class Program {
+//     static void Main(string[] args){
+//         bool a=false;
+//         bool b=true;
+//         if(a == b)
+//         Console.WriteLine("this will not print");
+//         else
+//         Console.WriteLine("this will print");
+//     }
+// }
+// public class UnitConverter
+// {
+//     int ratio;       //feild         //data member
+//         public UnitConverter (int unitRatio)   // Constructor      //function member
+//             {
+//                 ratio = unitRatio;
+//             } 
+//         public int Convert (int unit)  // Method         //function member
+//             {
+//                 return unit * ratio;
+//             } 
+// }
+
+// class Program {
+//     static void Main(string[] args){
+//         UnitConverter feetToInchesConverter = new UnitConverter (12); 
+//         int x=feetToInchesConverter.Convert(30);
+//         Console.WriteLine (x);
+//     }
+// }
+
+// public class Emp
+// {
+//     public string Name; // Instance field
+//     public static int Count; // Static field
+//     public Emp (string n) // Constructor
+//     {
+//         Name = n; // Assign the instance field
+//         Count = Count + 1; // Increment the static Count field
+//     }
+// }
+// class Program {
+//     static void Main(string[] args){
+//             Emp p1 = new Emp ("Ank");
+//             Emp p2 = new Emp ("it");
+//             Console.WriteLine (p1.Name);
+//             Console.WriteLine (p2.Name); 
+//             Console.WriteLine (Emp.Count);
+//     }
+// }
+
+// class Program {
+//     static void Main(string[] args){
+//             int x = 12345; // int is a 32-bit integer
+//             double y = x; // Implicit conversion to 64-bit integer
+//             short z = (short)x; // Explicit conversion to 16-bit integer
+//             Console.WriteLine (x);
+//             Console.WriteLine (y); 
+//             Console.WriteLine (z);
+//     }
+// }
+//     public class ValueType { 
+//         public int X; 
+//         public int Y; 
+//         }
+// class Program {
+//     static void Main(string[] args){
+//             ValueType v= new ValueType();
+//             v.X=7;
+//             ValueType v1= v;
+
+//             Console.WriteLine(v.X);
+//             Console.WriteLine(v1.X);
+
+//             v.X=9;
+//             Console.WriteLine(v.X);
+//             Console.WriteLine(v1.X);
+//     }
+// }
+
+class Program {
+    static void Main(string[] args){
+        
+    }
+}
+
